1577c6a12bd1ee436f361d02dd827681732e939c
ayush <ayush.goyal_cs18@gla.ac.in>
2020-03-26 13:15:18 +0530

    generating the constructor with both the parameters

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -10,6 +10,11 @@ public class Node<E> {
     private E data;
     private Node<E> next;
 
+    public Node(E data, Node<E> next) {
+        this.data = data;
+        this.next = next;
+    }
+
     public Node(E data) {
         this.data = data;
     }
