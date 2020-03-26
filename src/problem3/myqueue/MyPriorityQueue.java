dad75339609c2e7ac26f779ed999427dcc194b63
ayush <ayush.goyal_cs18@gla.ac.in>
2020-03-26 13:22:24 +0530

    creating the private field size integer type and initiliaze by 0. creating the private field front and rear of Node<E> type

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -7,9 +7,13 @@
 package problem3.myqueue;
 
 import problem3.adt.ADT;
+import problem3.node.Node;
 
 import java.util.Queue;
 public class MyPriorityQueue<E> implements ADT<E> {
+    private int size = 0;
+    private Node<E> front;
+    private Node<E> rear;
     @Override
     public void add(E data) {
 
