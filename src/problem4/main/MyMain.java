5b8183d4305554f1ad850d9ba942cb1c72cb4734
ayush <ayush.goyal_cs18@gla.ac.in>
2020-03-27 16:03:26 +0530

    defining the method traversePreOrder

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,7 +5,24 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+
+import problem1.node.TreeNode;
+import problem4.myqueue.MyQueue;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+
+    }
+
+    public static MyQueue traversePreOrder(TreeNode<Integer> currentNode, MyQueue<Integer> myQueue) {
+        if (currentNode != null) {
+            myQueue.enqueue(currentNode.getData());
+            System.out.print(currentNode.getData() + " ");
+            traversePreOrder(currentNode.getLeft(), myQueue);
+            traversePreOrder(currentNode.getRight(), myQueue);
+        }
+        return myQueue;
+    }
 }
