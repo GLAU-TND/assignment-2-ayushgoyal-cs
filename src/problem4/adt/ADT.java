d3cb6693f1a82b0ed1ffcda5ddb8430f440e2520
ayush <ayush.goyal_cs18@gla.ac.in>
2020-03-27 15:45:05 +0530

    implementing the methods

--- src/problem4/adt/ADT.java
+++ src/problem4/adt/ADT.java
@@ -1,4 +1,13 @@
 package problem4.adt;
 
 public interface ADT<E> {
+    public void enqueue(E data);
+
+    public E dequeue();
+
+    public E peek();
+
+    public boolean isEmpty();
+
+    public int getSize();
 }
