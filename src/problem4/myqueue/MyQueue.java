2e99a860006278f5df1b8fbd4a0c8236488d5944
ayush <ayush.goyal_cs18@gla.ac.in>
2020-03-27 15:50:25 +0530

    creating private field front,rear and size

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -7,5 +7,36 @@
 package problem4.myqueue;
 // to create queue to store pre - order successor
 
-public class MyQueue<E> {
+import problem3.node.Node;
+import problem4.adt.ADT;
+
+public class MyQueue<E> implements ADT<E> {
+    private Node<E> front;
+    private Node<E> rear;
+    private int size = 0;
+
+    @Override
+    public void enqueue(E data) {
+
+    }
+
+    @Override
+    public E dequeue() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
 }
